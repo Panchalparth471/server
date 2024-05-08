48ec93e50eb517d10cac7cea7750f6ea68c77bd7
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2024-05-08 18:49:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,19 @@ dotenv.config();
 
 const app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+    res.send('<h1>The Server is Up and Running</h1>');
+})
+
+
 
 mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
@@ -42,6 +52,3 @@ app.post('/api/users', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
